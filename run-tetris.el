ee4d4df509c71ca6c921f569e7376c7bd016d9e2
JenChieh <jcs090218@gmail.com>
2026-02-24 02:41:53 +0800

    add src

---
+++ run-tetris.el
@@ -0,0 +1,37 @@
+;;; run-tetris.el --- Play tetris  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/emacs-eine/run-tetris
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: games
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Play tetris.
+;;
+
+;;; Code:
+
+;; NOTE: Just the package file.
+
+(provide 'run-tetris)
+;;; run-tetris.el ends here
